db3bd58e3ac04b87df55bab1b3f11caf5c8455f0
boysbee <gogorenger@gmail.com>
2016-09-05 23:32:07 +0700

    Change generate api docs to use swagger2.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 #TODO API
+
+## Build
+`mvn clean package`
+
+## Run with Spring Boot
+`mvn spring-boot:run`
+
+## Run with JAR
+`java -jar target/api-todo-<version>.jar`
+
+## API Document
+`http://localhost:8090/swagger-ui.html`
+`http://localhost:8090/v2/api-docs`
--- src/main/java/com/exam/todo/Application.java
+++ src/main/java/com/exam/todo/Application.java
@@ -6,8 +6,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * Created by boysbee on 9/5/2016 AD.
@@ -15,7 +23,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAutoConfiguration  // Sprint Boot Auto Configuration
 @ComponentScan(basePackages = "com.exam.todo")
 @EnableJpaRepositories("com.exam.todo.dao.jpa") // To segregate MongoDB and JPA repositories. Otherwise not needed.
-@EnableSwagger // auto generation of API docs
+@EnableSwagger2
 public class Application {
     private static final Class<Application> applicationClass = Application.class;
     private static final Logger log = LoggerFactory.getLogger(applicationClass);
@@ -26,5 +34,25 @@ public class Application {
 
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(applicationClass);
+    }
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("greetings")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/todo/api/tasks.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("TODO REST API")
+                .description("TODO REST APIs")
+                .contact("gogorenger@gmail.com")
+                .license("MIT")
+                .licenseUrl("http://opensource.org/licenses/MIT")
+                .build();
     }
 }
